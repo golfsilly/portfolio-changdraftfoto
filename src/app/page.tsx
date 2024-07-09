73730abdbeb8e248128e0d70b9450922c8118641
golfsilly <pnpnd2107@gmail.com>
2024-07-09 22:15:56 +0700

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,11 @@ import Type from "@/components/home/Type";
 import { anton } from "@/config/fonts";
 import { cn } from "@/lib/utils";
 import Home1 from "@/components/home/Home1";
-import SlideImage from "@/components/home/SlideImage";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <Home1 />
-      {/* <SlideImage /> */}
     </main>
   );
 }
